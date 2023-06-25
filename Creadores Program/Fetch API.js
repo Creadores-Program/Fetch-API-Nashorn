5109aa8629730a78a3145011962b3c8098a98677
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2023-06-25 14:29:13 -0600

    Add files via upload
    POST added

--- Creadores Program/Fetch API.js
+++ Creadores Program/Fetch API.js
@@ -1,9 +1,12 @@
-function fetch(url, method, content){
+//Addon for Nashorn made by Creadores Program©2023
+//MIT license© https://raw.githubusercontent.com/Trollhunters501/Fetch-API-Nashorn/main/LICENSE
+function fetch(url, method, body){
     var HttpURLConnectionftch = Java.type('java.net.HttpURLConnection');
     var URLftch = Java.type('java.net.URL');
     var BufferedReaderftch = Java.type('java.io.BufferedReader');
     var InputStreamReaderftch = Java.type('java.io.InputStreamReader');
     var StringBufferftch = Java.type("java.lang.StringBuffer");
+    var StringBuilderftch = Java.type("java.lang.StringBuilder");
     var sendftch = {
         error: "[ERROR] ",
         info: "[INFO] ",
@@ -59,11 +62,35 @@ function fetch(url, method, content){
         return;
     }
     if(method == "POST"){
-        if(!content){
+        if(!body){
             console.error(sendftch.error + "You need content to use POST!");
             return;
         }
         console.warn(sendftch.warn + "This mode is Experimental and may not work correctly!");
-        return "Not supported";
+        var urlftch = new URLftch(url);
+        var conftch = urlftch.openConnection();
+        conftch.setRequestMethod("POST");
+        conftch.setRequestProperty("User-Agent", "Mozilla/5.0");
+        conftch.setRequestProperty("Content-Type", "application/json; utf-8");
+        var jsonContentftch = body;
+        conftch.setDoOutput(true);
+        var osftch = conftch.getOutputStream();
+        try{
+            var inputftch = jsonContentftch.getBytes("utf-8");
+            osftch.write(inputftch, 0, inputftch.length);
+        }finally{
+            osftch.close();
+        }
+        var brftch = new BufferedReaderftch(new InputStreamReaderftch(conftch.getInputStream(), "utf-8"));
+        try{
+            var responseftch = new StringBuilderftch();
+            var responseLineftch = null;
+            while((responseLineftch = brftch.readLine()) != null){
+                responseftch.append(responseLineftch.trim());
+            }
+        }finally{
+            brftch.close();
+        }
+        return responseftch.toString();
     }
 }
